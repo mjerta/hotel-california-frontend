878e751071fba311d0448dafa94f74e91fda964c
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-13 21:41:01 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    server: {
+        open: true, // Automatically opens the browser on server start
+        watch: {
+          usePolling: true, // Ensures changes are detected, useful in some environments
+        },
+      }
+})
