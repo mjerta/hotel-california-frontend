1657f6216e186fe5cd3936ed656411fa6a1eeb2a
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-14 23:22:29 +0200

    Add the overlayer for the shopping card button for the sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import {useState} from 'react';
+import shoppingcard from "../../assets/shopping-btn.svg"
 
 import "./SideBar.css"
 
 function SideBar() {
+  const [opensideBar, setOpensideBar] = useState(false);
+
   return (
-    <div className="sidebar">
-      <div className="sidebar-content">
-        <h1>up</h1>
-        <h1>bottom</h1>
+    <>
+      <div className={`sidebar ${opensideBar ? "sidebar-full-screen" : ""}`}>
+        <img
+          onClick={() => setOpensideBar(prev => !prev)}
+          className={"shoppingcard-img"}
+          src={shoppingcard} alt=""
+        />
+        {/*<div className={`overlay ${classname ? classname : ''}`}>*/}
+        <div
+          className={`sidebar-content}`}
+        >
+          <h1>up</h1>
+          <h1>bottom</h1>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
