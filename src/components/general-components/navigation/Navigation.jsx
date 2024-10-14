d5f367d225368f783463973d06cab2e5711a9502
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-15 01:32:25 +0200

    Add a bit of positioning for the absoulte elements in navigation

--- src/components/general-components/navigation/Navigation.jsx
+++ src/components/general-components/navigation/Navigation.jsx
@@ -1,5 +1,6 @@
 import "./Navigation.css"
 import hamburger from "../../../assets/hamburger-menu.svg";
+import profileImg from "../../../assets/user-circle.svg";
 import {OverlayContext} from "../../../context/OverlayProvider.jsx";
 import {useContext} from "react";
 
@@ -9,15 +10,14 @@ function Navigation({className}) {
     <nav className={className}>
       <img onClick={toggleOverlay} className={"hamburger-img"} src={hamburger}
            alt=""/>
+      <img className={"profile-img"} src={profileImg}/>
       <div className="navigation-content">
-        <h1>up</h1>
-        <h1>bottom</h1>
       </div>
-      {/*  hamburger component  - maybe imported*/}
-      {/*  profile button*/}
-      {/*  menu will be only visible on press of the button*/}
-      {/*  it will be full width and full height and then have to close*/}
-      {/*  on mobile the little side bar will be reduced to only the hamburger on the left side*/}
+        {/*  hamburger component  - maybe imported*/}
+        {/*  profile button*/}
+        {/*  menu will be only visible on press of the button*/}
+        {/*  it will be full width and full height and then have to close*/}
+        {/*  on mobile the little side bar will be reduced to only the hamburger on the left side*/}
     </nav>
   )
 }
