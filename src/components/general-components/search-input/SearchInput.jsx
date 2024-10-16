88c00d942ba957d3ffcf61214c8a89d3da26cd1c
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-16 17:03:00 +0200

    Add styling for the searchbar

--- src/components/general-components/search-input/SearchInput.jsx
+++ src/components/general-components/search-input/SearchInput.jsx
@@ -2,11 +2,13 @@ import "./SearchInput.css"
 
 function SearchInput({placeHolder, className}) {
   return (
-    <input
-      className={`search-input${className ? className : ''}`}
-      placeholder={placeHolder}
-      type={"text"}
-    />
+    <div className={"input-wrapper"}>
+      <input
+        className={`search-input${className ? className : ''}`}
+        placeholder={placeHolder}
+        type={"text"}
+      />
+    </div>
   )
 }
 
