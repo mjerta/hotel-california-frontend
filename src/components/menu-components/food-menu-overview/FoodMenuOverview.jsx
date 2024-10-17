e1ca93ba5e2388fd5465c5556104dcc15d4c8cf2
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-17 10:32:33 +0200

    Add some error handling for when there is not connection

--- src/components/menu-components/food-menu-overview/FoodMenuOverview.jsx
+++ src/components/menu-components/food-menu-overview/FoodMenuOverview.jsx
@@ -13,38 +13,37 @@ function FoodMenuOverview({className}) {
     async function fetchMeals() {
       try {
         setLoading(true);
-        const response = await axios.get(`${baseUrl}/api/v1/meals`)
+        const response = await axios.get(`${baseUrl}/api/v1/mseals`)
         console.log(response);
         setMeals(response.data)
       } catch (e) {
         console.error(e)
-
+        if (e.code ===  "ERR_NETWORK") {
+          setError("Could not make a connection with the API")
+        }
       } finally {
         setLoading(false);
       }
     }
-
     fetchMeals();
   }, [])
   return (
     <>
       <section className={`card-overview${className ? className : ''}`}>
-        {loading ? (
-          <>
-            <p>Loading API ...</p>
-          </>
+        {error ? (
+          <p>{error}</p>
+        ) : loading ? (
+          <p>Loading API ...</p>
         ) : (
-          <>
-            {meals.length > 0 && meals.map((meal) => (
-              <Card
-                key={meal.id}
-                name={meal.name}
-                description={meal.description}
-                price={meal.price}
-                image={meal.image}
-              />
-            ))}
-          </>
+          meals.length > 0 && meals.map((meal) => (
+            <Card
+              key={meal.id}
+              name={meal.name}
+              description={meal.description}
+              price={meal.price}
+              image={meal.image}
+            />
+          ))
         )}
       </section>
     </>
