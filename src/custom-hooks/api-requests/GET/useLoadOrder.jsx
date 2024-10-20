12c73f3c2be76d4fd4a49d21de01cc72e0ebb31f
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-20 16:17:30 +0200

    Add an interval for checking status and order, also remove it when payed

--- src/custom-hooks/api-requests/GET/useLoadOrder.jsx
+++ src/custom-hooks/api-requests/GET/useLoadOrder.jsx
@@ -4,11 +4,13 @@ import axios from 'axios';
 function useLoadOrder(setCurrentOrder, setStatus, setCurrentLocation, token) {
   const baseUrl = import.meta.env.VITE_API_URL;
   useEffect(() => {
+
     const localOrderReference = localStorage.getItem("orderReference");
     const localOrderId = localStorage.getItem("id");
 
-    if (localOrderReference) {
-      async function loadOrderWithOrderReference() {
+    async function fetchOrder() {
+      if (localOrderReference) {
+
         try {
           console.log(localOrderReference);
           const response = await axios.get(`${baseUrl}/api/v1/orders/orderreference`, {
@@ -16,17 +18,25 @@ function useLoadOrder(setCurrentOrder, setStatus, setCurrentLocation, token) {
           });
           const data = response.data;
           console.log(data);
-          setCurrentOrder(data.meals);
-          setStatus(data.status);
-          setCurrentLocation(data.destination.locationNumber);
+          // Check if the order status is ORDER_PAYED
+          if (data.status === "ORDER_PAYED") {
+            // Reset everything
+            setCurrentOrder([]);
+            setStatus(null);
+            setCurrentLocation(null);
+            localStorage.removeItem("orderReference"); // Optionally clear local storage
+            clearInterval(intervalId)
+          } else {
+            // Update state with fetched data
+            setCurrentOrder(data.meals);
+            setStatus(data.status);
+            setCurrentLocation(data.destination.locationNumber);
+          }
         } catch (e) {
           console.error(e);
         }
-      }
 
-      loadOrderWithOrderReference();
-    } else if (localOrderId && token) {
-      async function loadOrderWithId() {
+      } else if (localOrderId && token) {
         try {
           console.log(localOrderId);
           const response = await axios.get(`${baseUrl}/api/v1/orders/${localOrderId}`, {
@@ -43,9 +53,15 @@ function useLoadOrder(setCurrentOrder, setStatus, setCurrentLocation, token) {
           console.error(e);
         }
       }
-
-      loadOrderWithId();
     }
+    fetchOrder();
+
+    // Set up interval for updates every 10 seconds
+    const intervalId = setInterval(fetchOrder, 10000);
+
+
+    // Clean up the interval on unmount
+    return () => clearInterval(intervalId);
   }, [token]); // Only depend on token
 };
 
