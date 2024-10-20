6a75c6fa4aa1971ef7886ceb55edaee995df414f
Maarten Postma <maarten.postma1000@gmail.com>
2024-10-20 22:42:24 +0200

    Add a small counter element to the shopping card image

--- src/components/general-components/sidebar/SideBar.jsx
+++ src/components/general-components/sidebar/SideBar.jsx
@@ -1,29 +1,41 @@
-import {useState} from 'react';
+import {useContext, useState} from 'react';
 import shoppingcard from "../../../assets/shopping-btn.svg"
 
 import "./SideBar.css"
 import "./Variants.css"
+import {OrderContext} from "../../../context/OrderProvider.jsx";
 
 function SideBar({className, children}) {
   const [openSideBar, setOpenSideBar] = useState(false);
+  const {currentOrder} = useContext(OrderContext);
 
-  return (
-    <>
-      <img
-        onClick={() => setOpenSideBar(prev => !prev)}
-        className={`shoppingcard-img ${openSideBar ? "shopping-card-img-enforce" : ""}`}
-        src={shoppingcard} alt=""
-      />
-      <aside
-        className={`sidebar ${className} ${openSideBar ? "sidebar-full-screen" : ""}`}>
-        <div
-          className={`sidebar-content ${openSideBar ? "open-sidebar-content" : ''}`}
-        >
-          {children}
-        </div>
-      </aside>
-    </>
-  )
+  console.log(currentOrder.length)
+
+  return (<>
+    <div className={`img-container-shopping-card ${openSideBar ? "shopping-card-img-enforce" : ""}`}>
+      <div className="inside-img-container">
+        {currentOrder.length > 0 && (
+          <span className="order-counter">
+            {currentOrder.length}
+          </span>
+        )}
+        <img
+          onClick={() => setOpenSideBar(prev => !prev)}
+          className={`shoppingcard-img`}
+          src={shoppingcard} alt="shopping card image"
+        />
+      </div>
+    </div>
+    <aside
+      className={`sidebar ${className} ${openSideBar ? "sidebar-full-screen" : ""}`}
+    >
+      <div
+        className={`sidebar-content ${openSideBar ? "open-sidebar-content" : ''}`}
+      >
+        {children}
+      </div>
+    </aside>
+  </>)
 }
 
 export default SideBar;
