10209743a5f99909fa0d225cd970d108f6deaa35
Maarten Postma <maarten.postma1000@gmail.com>
2024-11-07 23:20:54 +0100

    Remove Mui libary and install rechart instead according to quickscan of the assignment

--- src/components/manager-dashboard-components/bar-chart-section/BarChartSection.jsx
+++ src/components/manager-dashboard-components/bar-chart-section/BarChartSection.jsx
@@ -1,29 +1,25 @@
-import "./BarChartSection.css"
-import {BarChart} from "@mui/x-charts";
+import "./BarChartSection.css";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from "recharts";
 
-function BarChartSection({className, menuItems, data}) {
+function BarChartSection({ className, menuItems, data }) {
+  // Prepare the data in the format Recharts expects
+  const chartData = menuItems.map((item, index) => ({
+    name: item,
+    value: data[index] || 0,
+  }));
+
   return (
-    <div className={`bar-chart-section ${className ? className : ''}`}>
-      <BarChart
-        sx={
-        {bgcolor: '#d0d5dd' }
-        }
-        xAxis={[
-          {
-            id: 'barCategories',
-            data: menuItems,
-            scaleType: 'band',
-          },
-        ]}
-        series={[
-          {
-            color: '#37323e',
-            data: data
-          },
-        ]}
-      />
+    <div className={`bar-chart-section ${className || ""}`}>
+      <ResponsiveContainer fill={"green"} width="100%" height={300}>
+        <BarChart  data={chartData} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="value" fill="#37323e" />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
-  )
+  );
 }
 
 export default BarChartSection;
